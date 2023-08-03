64d6d7774a16424ee0e1025f9e09560c22341945
takuteh <takumi.ktym@gmail.com>
2023-08-03 11:18:34 +0900

    c++書き直し1回目

--- motor.cpp
+++ motor.cpp
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include<iostream>
 #include<stdlib.h>
 #include<strings.h>
 #include <sys/types.h>
@@ -9,24 +9,85 @@
 #include <unistd.h>
 #include <linux/i2c-dev.h>
 
-#define MdrvAdr1 0x60
-#define MdrvAdr2 0x64
+using std::cout;
+using std::endl;
+using std::string;
+
+#define MdrvAddr1 0x60
+#define MdrvAddr2 0x64
 #define I2C_DevName  "/dev/i2c-1"
 
-int main(){
-    int fd;
+class Motor{
+    public:
+        int fd;
+        char buf[2];
+        Motor();
+        void drv1_slave();
+        void drv2_slave();
+        void rotate(string drv,string direction);
+    private:
+        const int i2cAddr1=MdrvAddr1;
+        const int i2cAddr2=MdrvAddr2;
 
-    	if ((fd = open(i2cDevName, O_RDWR)) < 0) {
+        const int forward=0x01;
+        const int backward=0x10;
+        const int stop=0x11;
+        const int neutral=0x00;
+};
+
+Motor::Motor(){
+    	if ((this->fd = open(I2C_DevName, O_RDWR)) < 0) {
 		printf("Faild to open i2c port! ><\n");
-		return 1;
-	}
+         }
+        buf[0]=0x00;
 
-	if (ioctl(fd, I2C_SLAVE, i2cAddr1) < 0) {
-		printf("Faild to open port...\n");
-		return 1;
-	}
-while(1){
-    write(fd,0x02,2);
 }
+
+void Motor::drv1_slave(){
+	if (ioctl(this->fd, I2C_SLAVE, i2cAddr1) < 0) {
+		printf("Faild to open port...\n");
+	}
+}
+
+void Motor::drv2_slave(){
+    	if (ioctl(this->fd, I2C_SLAVE, i2cAddr2) < 0) {
+		printf("Faild to open port...\n");
+//		return 1;
+	}
+}
+
+void Motor::rotate(string drv,string direction){
+buf[1]=0;
+if(drv=="drv1"){
+this->drv1_slave();
+}else if(drv=="drv2"){
+this->drv2_slave();
+}else{
+cout<<drv<<" is invalid string!!"<<endl;
+}
+    if(direction=="forward"){
+        buf[1]=0x09<<2|this->forward;
+    }else if(direction=="backward"){
+        buf[1]=0x09<<2|this->backward;
+    }else if(direction=="stop"){
+        buf[1]=0x09<<2|this->stop;
+    }else if(direction=="neutral"){
+        buf[1]=0x00;
+    }
+
+    if(write(fd,buf,2)!=2){
+    cout<<"error"<<endl;
+    }
+}
+
+int main(){
+char buf[2];
+int fd;
+    Motor motor;
+    motor.rotate("drv1","forward");
+    motor.rotate("drv2","forward");
+    sleep(2);
+    motor.rotate("drv1","neutral");
+    motor.rotate("drv2","neutral");
 return 0;
 }
